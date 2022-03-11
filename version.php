142cca0540ab7c9027939613741994de291a5ff3
Menrath André <andre.menrath@uni-graz.at>
2022-03-11 11:54:43 +0100

    push version for testing the ci pipeline

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2022021000;
+$plugin->version = 2022021001;
 $plugin->requires = 2014051200;
 $plugin->component = 'atto_wordcount';
 $plugin->release = '1.2.0';
