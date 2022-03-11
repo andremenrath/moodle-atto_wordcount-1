96b821d32e1ff59419cbb92c62fe50fa880a0d85
Menrath André <andre.menrath@uni-graz.at>
2022-03-11 12:40:19 +0100

    trigger pipeline

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2022021003;
+$plugin->version = 2022021004;
 $plugin->requires = 2014051200;
 $plugin->component = 'atto_wordcount';
 $plugin->release = '1.2.0';
